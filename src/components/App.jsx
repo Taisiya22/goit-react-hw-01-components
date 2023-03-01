8b0310fdb6bebe4115ab5bab585877de4e62a5ae
Taisiya Tomashevska <taya22011985@gmail.com>
2023-03-01 19:22:26 +0200

    bb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,23 +15,23 @@
 //   );
 // };
 
-// import { Profile } from '../components/Profile/Profile'
-// import { Statistics } from '../components/Statistic/Statistic'
-// import user from 'json/user.json'
-// import data from 'json/data.json'
+import { Profile } from '../components/Profile/Profile'
+import { Statistics } from '../components/Statistic/Statistic'
+import user from 'json/user.json'
+import data from 'json/data.json'
 
-// export function App() { 
-//   return (
-//     <div>
-//     <Profile
-//   username={user.username}
-//   tag={user.tag}
-//   location={user.location}
-//   avatar={user.avatar}
-//   stats={user.stats}
-//     />
-//       <Statistics title="Upload stats" stats={data} />
+export function App() { 
+  return (
+    <div>
+    <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+    />
+      <Statistics title="Upload stats" stats={data} />
      
-// </div>
-//   )
-// }
+</div>
+  )
+}
